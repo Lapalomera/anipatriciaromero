da2cba134df1ce8c53e49ddc9828c53721890e89
unknown <fidelitojunio@gmail.com>
2023-02-15 20:09:23 -0400

    img ruta solo asset

--- src/app/layout/galery/galery.component.ts
+++ src/app/layout/galery/galery.component.ts
@@ -19,17 +19,17 @@ arepa:Product[]=[
     {
       name:'La Coronada',
       ingredients:['Mezcla de Pollo','Tajada','Queso Amarillo'],
-      img:'/assets/img/arepa/paloma1.jpeg',
+      img:'assets/img/arepa/paloma1.jpeg',
     },
     {
       name:'La Catira',
       ingredients:['Pollo Mechado','Queso Amarillo'],
-      img:'/assets/img/arepa/paloma2.jpeg',
+      img:'assets/img/arepa/paloma2.jpeg',
     },
     {
       name:'La Pelua',
       ingredients:['Carne Mechada','Queso Amarillo'],
-      img:'/assets/img/arepa/paloma3.jpeg',
+      img:'assets/img/arepa/paloma3.jpeg',
     },
     {
       name:'La Sifrina',
@@ -44,17 +44,17 @@ arepa:Product[]=[
     {
       name:'La Capresa',
       ingredients:['Tomate','Oregano','Queso Mozzarela'],
-      img:'/assets/img/arepa/paloma6.jpeg',
+      img:'assets/img/arepa/paloma6.jpeg',
     },
     {
       name:'La Pecadora',
       ingredients:['Diablito','Tomate','Tajada','Huevo','Queso'],
-      img:'/assets/img/arepa/paloma7.jpeg',
+      img:'assets/img/arepa/paloma7.jpeg',
     },
     {
       name:'La Tunning',
       ingredients:['Arun','Tomate','Aguacate'],
-      img:'/assets/img/arepa/paloma8.jpeg',
+      img:'assets/img/arepa/paloma8.jpeg',
     }
     
    ];
@@ -63,16 +63,16 @@ arepa:Product[]=[
    {
     title:'San Valentin',
     description:'Enamora a tu Pareja con un Desayuno',
-    img:'/assets/img/news/san_valentin.jpeg'
+    img:'assets/img/news/san_valentin.jpeg'
    },
    {
     title:'Serie del Caribe',
     description:'Botala de Home Run como Jose Rondon para ganarte una palomera',
-    img:'/assets/img/news/serie_caribe.jpeg'
+    img:'assets/img/news/serie_caribe.jpeg'
    },{
     title:'Claramente la Palomera es lo mejor',
     description:'la palomera ya no llora la palomera factura',
-    img:'/assets/img/news/claramente.jpeg'
+    img:'assets/img/news/claramente.jpeg'
    }
   ];
    
