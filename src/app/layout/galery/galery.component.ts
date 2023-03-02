dcfa33ea4abc5dd33abaf25543aab3f330b80f91
unknown <fidelitojunio@gmail.com>
2023-03-01 23:15:40 -0400

    anis

--- src/app/layout/galery/galery.component.ts
+++ src/app/layout/galery/galery.component.ts
@@ -19,7 +19,7 @@ arepa:Product[]=[
     {
       name:'La Coronada',
       ingredients:['Mezcla de Pollo','Tajada','Queso Amarillo'],
-      img:"/anipatriciaromero/assets/img/arepa/paloma1.JPEG",
+      img:"assets/img/arepa/paloma1.JPEG",
     },
     {
       name:'La Catira',
