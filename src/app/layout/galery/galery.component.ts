215d7ea80987dc99fe592fc7bd4aa97328ad6818
unknown <fidelitojunio@gmail.com>
2023-03-01 23:03:57 -0400

    image extension

--- src/app/layout/galery/galery.component.ts
+++ src/app/layout/galery/galery.component.ts
@@ -19,42 +19,42 @@ arepa:Product[]=[
     {
       name:'La Coronada',
       ingredients:['Mezcla de Pollo','Tajada','Queso Amarillo'],
-      img:"assets/img/arepa/paloma1.jpeg",
+      img:"assets/img/arepa/paloma1.JPEG",
     },
     {
       name:'La Catira',
       ingredients:['Pollo Mechado','Queso Amarillo'],
-      img:"assets/img/arepa/paloma2.jpeg",
+      img:"assets/img/arepa/paloma2.JPEG",
     },
     {
       name:'La Pelua',
       ingredients:['Carne Mechada','Queso Amarillo'],
-      img:"assets/img/arepa/paloma3.jpeg",
+      img:"assets/img/arepa/paloma3.JPEG",
     },
     {
       name:'La Sifrina',
       ingredients:['Queso Amarillo','Jamon'],
-      img:"assets/img/arepa/paloma4.jpeg",
+      img:"assets/img/arepa/paloma4.JPEG",
     },
     {
       name:'Domino',
       ingredients:['Caraota','Platano','Queso'],
-      img:"assets/img/arepa/paloma5.jpeg",
+      img:"assets/img/arepa/paloma5.JPEG",
     },
     {
       name:'La Capresa',
       ingredients:['Tomate','Oregano','Queso Mozzarela'],
-      img:"assets/img/arepa/paloma6.jpeg",
+      img:"assets/img/arepa/paloma6.JPEG",
     },
     {
       name:'La Pecadora',
       ingredients:['Diablito','Tomate','Tajada','Huevo','Queso'],
-      img:"assets/img/arepa/paloma7.jpeg",
+      img:"assets/img/arepa/paloma7.JPEG",
     },
     {
       name:'La Tunning',
       ingredients:['Arun','Tomate','Aguacate'],
-      img:"assets/img/arepa/paloma8.jpeg",
+      img:"assets/img/arepa/paloma8.JPEG",
     }
     
    ];
@@ -63,16 +63,16 @@ arepa:Product[]=[
    {
     title:'San Valentin',
     description:'Enamora a tu Pareja con un Desayuno',
-    img:"assets/img/news/san_valentin.jpeg"
+    img:"assets/img/news/san_valentin.JPEG"
    },
    {
     title:'Serie del Caribe',
     description:'Botala de Home Run como Jose Rondon para ganarte una palomera',
-    img:"assets/img/news/serie_caribe.jpeg"
+    img:"assets/img/news/serie_caribe.JPEG"
    },{
     title:'Claramente la Palomera es lo mejor',
     description:'la palomera ya no llora la palomera factura',
-    img:"assets/img/news/claramente.jpeg"
+    img:"assets/img/news/claramente.JPEG"
    }
   ];
    
